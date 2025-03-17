8e86a4336fa88e92c538d4e1b174586b5a846e7d
Exitus-Letalis <andreevic964@gmail.com>
2025-03-18 01:50:27 +0200

    Додавання імя користувача

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -10,14 +10,20 @@ return(
     <div className={styles.body}>
         <div className={styles.mainlayers}>
             <div className={styles.upimage}></div>
-            <div className={styles.avatar}></div>
-                    <div className={styles.redagbutton}>
-                        <div className={styles.buttonredagpros}>Редагувати профіль</div>
-                        <div className={styles.buttonlook}>Переглянути архів</div>
+            <div className={styles.maininfo}>
+             <div className={styles.avatar}></div>
+                 <div className={styles.information}>
+                        <div className={styles.name}>@Slavedick</div>
+                        <div className={styles.infor}>Kalush</div>
+                  </div>
+                  </div>
+                <div className={styles.redagbutton}>
+                    <div className={styles.buttonredagpros}>Редагувати профіль</div>
+                    <div className={styles.buttonlook}>Переглянути архів</div>
                     </div>
+                     
             <div className={styles.prof}>
-                <div className={styles.name}>Name</div>
-                <div className={styles.infor}>Inform</div>
+                
             </div>
             <div className={styles.public}>
                 <div className={styles.savepubl}>
