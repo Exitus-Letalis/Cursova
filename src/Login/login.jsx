8ed50316c6e45f4cc7805a9f44d00742218747ff
Exitus-Letalis <andreevic964@gmail.com>
2025-03-19 11:42:54 +0200

    Зміна табуляція

--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import styles from './log.module.scss';  // Стилі для Log компонента
-import lock from './lock.png';
-import user from './user.png';
-import modal from './reg2.module.scss';
-import { Link } from 'react-router-dom';
-
-
+import styles from "./log.module.scss"; // Стилі для Log компонента
+import lock from "./lock.png";
+import user from "./user.png";
+import modal from "./reg2.module.scss";
+import { Link } from "react-router-dom";
 
 const Log = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -19,62 +17,89 @@ const Log = () => {
     setIsModalVisible(true);
   };
 
-  
-
   return (
     <div className={styles.Body}>
       <div className={styles.Wrap}>
         <h1 className={styles.h1}>CutieGram</h1>
-       
+
         <div className={styles.box}>
-          <input className={styles.input} type='text' placeholder='Username' required />
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Username"
+            required
+          />
           <img className={styles.userpng} src={user} />
         </div>
         <div className={styles.box}>
-          <input className={styles.input} type='password' placeholder='Password' required />
-          <img     className={styles.lockpng} src={lock} />
+          <input
+            className={styles.input}
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <img className={styles.lockpng} src={lock} />
         </div>
         <div className={styles.remember}>
-          <label className={styles.label}><input type='checkbox' />Запам'ятати мене </label>
-          <a className={styles.a} href="#">Забули пароль? </a>
+          <label className={styles.label}>
+            <input type="checkbox" />
+            Запам'ятати мене{" "}
+          </label>
+          <a className={styles.a} href="#">
+            Забули пароль?{" "}
+          </a>
         </div>
-        <Link to='/mainpage'>
-        <button className={styles.button} type='submit' >Увійти</button>
+        <Link to="/mainpage">
+          <button className={styles.button} type="submit">
+            Увійти
+          </button>
         </Link>
       </div>
       <div className={styles.registr}>
-        <p >Не маєте аккаунту? </p> 
-        <p onClick={handleModalOpen} className={styles.rega}>Зареєструватися</p>
+        <p>Не маєте аккаунту? </p>
+        <p onClick={handleModalOpen} className={styles.rega}>
+          Зареєструватися
+        </p>
       </div>
-      
+
       {/* Модальне вікно */}
       {isModalVisible && (
         <div className={styles.modal2}>
           <div className={styles.modalContent2}>
-          <h1 className={styles.h1}>CutieGram</h1>
+            <h1 className={styles.h1}>CutieGram</h1>
             {/* Кнопка для закриття */}
-            <button className={styles.close2} onClick={handleModalClose}>&times;</button>
-              <form>
-                <div className={styles.box}>
-                  <input className={styles.input} 
-                    type='text' 
-                    placeholder='Username' 
-                    required/>
-                </div>
-                <div className={styles.box}>
-                  <input className={styles.input} 
-                    type='password' 
-                    placeholder='Password' 
-                    required/>
-                </div>
-                <div className={styles.box}>
-                  <input className={styles.input} 
-                    type='Email' 
-                    placeholder='Email' 
-                    required/>
-                </div>
-                    <button className={styles.button} type='submit' >Зареєструватися</button>
-              </form>
+            <button className={styles.close2} onClick={handleModalClose}>
+              &times;
+            </button>
+            <form>
+              <div className={styles.box}>
+                <input
+                  className={styles.input}
+                  type="text"
+                  placeholder="Username"
+                  required
+                />
+              </div>
+              <div className={styles.box}>
+                <input
+                  className={styles.input}
+                  type="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <div className={styles.box}>
+                <input
+                  className={styles.input}
+                  type="Email"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+              <button className={styles.button} type="submit">
+                Зареєструватися
+              </button>
+            </form>
           </div>
         </div>
       )}
@@ -83,5 +108,3 @@ const Log = () => {
 };
 
 export default Log;
-
-
--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,50 +1,37 @@
 import React from "react";
-import styles from './profile.module.scss';
+import styles from "./profile.module.scss";
 
-
-const Profile= () =>{
-
-
-
-return(
+const Profile = () => {
+  return (
     <div className={styles.body}>
-        <div className={styles.mainlayers}>
-            <div className={styles.upimage}></div>
-            <div className={styles.maininfo}>
-             <div className={styles.avatar}></div>
-                 <div className={styles.information}>
-                        <div className={styles.name}>@Slavedick</div>
-                        <div className={styles.infor}>Kalush</div>
-                  </div>
-                  </div>
-                <div className={styles.redagbutton}>
-                    <div className={styles.buttonredagpros}>Редагувати профіль</div>
-                    <div className={styles.buttonlook}>Переглянути архів</div>
-                    </div>
-                     
-            <div className={styles.prof}>
-                
-            </div>
-            <div className={styles.public}>
-                <div className={styles.savepubl}>
-                    text
-                </div>
-                <div className={styles.foto}>
-                    Місце для фото
-                </div>
-            </div>
+      <div className={styles.mainlayers}>
+        <div className={styles.upimage}></div>
+        <div className={styles.maininfo}>
+          <div className={styles.avatar}></div>
+          <div className={styles.information}>
+            <div className={styles.name}>@Slavedick</div>
+            <div className={styles.infor}>Kalush</div>
+          </div>
         </div>
+        <div className={styles.redagbutton}>
+          <div className={styles.buttonredagpros}>Редагувати профіль</div>
+          <div className={styles.buttonlook}>Переглянути архів</div>
+        </div>
+
+        <div className={styles.prof}>
+          <div className={styles.public}>
+            <div className={styles.savepubl}>
+              text(це верхня полоска з можливістю перемикання)
+            </div>
+            <div className={styles.foto}>
+              Місце для фото(потрібно придумати алгоритм аби всі фото які
+              додаються ставали у таблицю)
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
-)
-
-
-
-}
-
-
-
-
-
+  );
+};
 
 export default Profile;
